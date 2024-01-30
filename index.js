27b5a9eb5ab1f631e38db634bf18a2e3a7574d67
rsvincent <rvincent87@gmail.com>
2024-01-29 22:17:16 -0500

    tutorial mongo&mongoose

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://rvincent87:cRhon4065@cluster0.iiml9ct.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// import mongoose from "mongoose";
+
+// mongoose.connect(
+//   "mongodb+srv://rvincent87:cRhon4065@cluster0.iiml9ct.mongodb.net/"
+// );
